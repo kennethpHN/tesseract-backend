42250e5de75461dbb1005ec72af2963b8056fe3b
Kenneth Pineda <kennethpineda11@gmail.com>
2022-10-16 01:47:07 -0600

    added /status request handler

--- src/index.js
+++ src/index.js
@@ -11,6 +11,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
+app.get("/status",(req, res)=>{
+    res.send({status: "OK"});
+});
+
 app.listen(3000, ()=>{
     console.log("app running: ");
     initDB().then(()=>{
